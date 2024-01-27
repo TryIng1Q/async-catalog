14310743375a571f7cf3868f4d7248fcd39f3c9c
TryIng1Q <kpktdhovord@gmail.com>
2024-01-27 21:18:26 +0600

    add some function

--- js/episodes-list.js
+++ js/episodes-list.js
@@ -1,6 +1,12 @@
 // Import
 import logicFunction from "./logic.js";
 
+// Init CSS style
+const currentPage = window.location.pathname;
+console.log(logicFunction.loadCSSstyle(currentPage));
+
+
+
 // Create episodes list
 const episodesContainer = document.getElementById('episodes-container');
 logicFunction.showEpisodesList();
--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,24 @@
 import visualFunction from "./visual.js";
 
 const logicFunction = {
+	uploadedCSSstyles: {
+		'/episodes-list-page': 'styles/episodes-list.css',
+		'/episode-info-page': 'styles/episode-info.css',
+	},
+	async loadCSSstyle(url) {
+		// if (url) return this.uploadedCSSstyles[url];
+		
+		new Promise(resolve => {
+			const cssLink = document.createElement('link');
+			cssLink.rel = 'stylesheet';
+			cssLink.href = 'styles/episodes-list.css';
+
+			cssLink.addEventListener('load', () => {
+				resolve();
+			});
+			document.head.append(cssLink);
+		});
+	},
 	async showEpisodesList() {
 		const episodesObj = await fetch('https://swapi.dev/api/films').then(data => data.json());
 
